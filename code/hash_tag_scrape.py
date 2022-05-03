f7ba9e6ac7f13257e3cfeccc5d74032b9834319a
saivishwakgangam <gangamsaivishwak@gmail.com>
2022-05-03 18:17:37 +0530

    Adding Up Punjab Hashtag

--- code/hash_tag_scrape.py
+++ code/hash_tag_scrape.py
@@ -1,4 +1,4 @@
 import snscrape.modules.twitter as sntwitter
 import pandas
 import os
-os.system("snscrape --jsonl --max-results 50000 --since 2021-12-01 twitter-hashtag HijabRow > ../hash_tags_tweets/HijabRow.json")
+os.system("snscrape --jsonl --max-results 100000 --since 2021-12-01 twitter-hashtag BravePmModi > ./hash_tags_tweets/BravePmModi.json")
